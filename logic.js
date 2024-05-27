dc484ed13029f9f490eb20b967bb23d795cd8c13
Zied <73042811+ZiedDev@users.noreply.github.com>
2024-05-27 16:32:54 +0300

    update with latest exams and add links to mcq mate

--- logic.js
+++ logic.js
@@ -489,3 +489,103 @@ function watchForHover() {
 }
 
 watchForHover()
+
+
+function createModal(title, contents, submitButtonBehavior, cancelButtonBehavior) {
+    const modalContainer = document.createElement('div')
+    const modal = document.createElement('div')
+    const backdrop = document.createElement('div')
+    const modalButtonsContainer = document.createElement('div')
+    const modalTitle = document.createElement('h1')
+    modalTitle.textContent = title
+
+    modalContainer.appendChild(modal)
+    modalContainer.appendChild(backdrop)
+    modal.appendChild(modalTitle)
+
+    for (let i = 0; i < contents.length; i++) {
+        const modalContent = document.createElement('h2')
+        modalContent.innerHTML = typeof (contents[i]) == 'string' ? contents[i] : contents[i][0]
+
+        if (typeof (contents[i][1]) == 'function') {
+            contents[i][1]()
+        }
+
+        modal.appendChild(modalContent)
+    }
+
+    modalContainer.classList.add('modal-container')
+    modalContainer.id = 'modal-container'
+    modal.classList.add('modal')
+    backdrop.classList.add('backdrop')
+
+    backdrop.addEventListener('click', () => {
+        modalContainer.parentNode.removeChild(modalContainer)
+    })
+
+    document.addEventListener('keydown', e => {
+        if (e.key == 'Escape') {
+            if (modalContainer.parentNode == null) {
+                return
+            }
+
+            modalContainer.parentNode.removeChild(modalContainer)
+        }
+
+    })
+
+    modalButtonsContainer.classList.add('modal-buttons-container')
+    modal.appendChild(modalButtonsContainer)
+
+    if (submitButtonBehavior != undefined) {
+        const submitButton = document.createElement('button')
+        submitButton.textContent = submitButtonBehavior[0]
+
+        submitButton.addEventListener('click', () => {
+            submitButtonBehavior[1]()
+            modalContainer.parentNode.removeChild(modalContainer)
+        })
+
+        modalButtonsContainer.appendChild(submitButton)
+    }
+
+    if (cancelButtonBehavior != undefined) {
+        const cancelButton = document.createElement('button')
+        cancelButton.textContent = cancelButtonBehavior[0]
+
+        cancelButton.addEventListener('click', () => {
+            cancelButtonBehavior[1]()
+            modalContainer.parentNode.removeChild(modalContainer)
+        })
+
+        modalButtonsContainer.appendChild(cancelButton)
+    }
+
+    document.body.appendChild(modalContainer)
+
+    return modalContainer
+}
+
+if (localStorage.getItem('hidePopup') == null) {
+    localStorage.setItem('hidePopup', 'false')
+}
+if (localStorage.getItem('hidePopup') == 'false') {
+    createModal(
+        'MCQ Mate is Here!!!',
+        [
+            'MCQ Mate is an updated version for the Cambridge MCQ Checker with more features and cleaner layout',
+            `<span style="display: flex; align-items: center; gap: 0.5rem;">Do not show this popup again? <input type="checkbox" id="hide-popup-check" onclick="localStorage.setItem('hidePopup', 'true')"></span>`,
+        ],
+        [
+            'Open MCQ Mate',
+            () => {
+                window.open('http://zieddev.github.io/mcq-mate/', "_self")
+            },
+        ], [
+        'Close',
+        () => {
+
+        },
+    ]
+    )
+}
